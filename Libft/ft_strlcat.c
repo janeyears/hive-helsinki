7629772c1b2ad35656d9bc8605f14155008541a4
Evgeniia Kashirskaia <ekashirs@c1r1p8.hive.fi>
2024-10-29 17:03:45 +0200

    add new functions

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ekashirs <ekashirs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/29 16:24:26 by ekashirs          #+#    #+#             */
-/*   Updated: 2024/10/29 16:55:36 by ekashirs         ###   ########.fr       */
+/*   Created: 2024/10/29 16:56:06 by ekashirs          #+#    #+#             */
+/*   Updated: 2024/10/29 16:57:35 by ekashirs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcpy(char *dest, const char *src, size_t size)
+size_t strlcat( char *dest, const char *src, size_t size )
 {
-	size_t	i;
-
-	i = 0;
-
-	while (src[i] != 0 && i < size - 1)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	while (src[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+    
 }
