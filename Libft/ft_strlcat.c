2498541eeb9c232161eceea0478b43939afda155
Jane Years <janeletoxx@gmail.com>
2024-10-30 21:47:31 +0200

    add new functions

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -12,7 +12,25 @@
 
 #include "libft.h"
 
-size_t strlcat( char *dest, const char *src, size_t size )
+size_t ft_strlcat( char *dest, const char *src, size_t size )
 {
-    
+    size_t	len_d;
+	size_t	len_s;
+	size_t  i;
+
+	len_d = ft_strlen(dest);
+	len_s = ft_strlen(src);
+	i = 0;
+
+    if (size == 0)
+        return len_s;
+	if (size <= len_d)
+		return (len_s + size);
+	while (len_d + i < size - 1 && src[i] != '\0')
+	{
+		dest[len_d + i] = src[i];
+		i++;
+	}
+	dest[len_d + i] = '\0';
+	return (len_d + len_s);
 }
