5e97afa6c94d55b8b3aea75f786aba970f2da329
Evgeniia Kashirskaia <ekashirs@c2r4p9.hive.fi>
2024-11-01 16:21:15 +0200

    add new functions

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,11 +6,56 @@
 /*   By: ekashirs <ekashirs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 11:40:44 by ekashirs          #+#    #+#             */
-/*   Updated: 2024/11/01 12:42:02 by ekashirs         ###   ########.fr       */
+/*   Updated: 2024/11/01 16:14:48 by ekashirs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t	ft_countword(char const *s, char c)
+{
+	size_t	count;
+
+	if (!*s)
+		return (0);
+	count = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+			count++;
+		while (*s != c && *s)
+			s++;
+	}
+	return (count);
+}
+
+
 char	**ft_split(char const *s, char c)
 {
-	
+	char	**list;
+	size_t	word_len;
+	int		i;
+
+	list = (char **)malloc((ft_countword(s, c) + 1) * sizeof(char *));
+	if (!s || !list)
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s)
+		{
+			if (!ft_strchr(s, c))
+				word_len = ft_strlen(s);
+			else
+				word_len = ft_strchr(s, c) - s;
+			list[i++] = ft_substr(s, 0, word_len);
+			s += word_len;
+		}
+	}
+	list[i] = NULL;
+	return (list);
 }
