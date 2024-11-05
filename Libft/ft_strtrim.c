ca1a57786731b61f785a526a18f250e827339c20
Evgeniia Kashirskaia <ekashirs@c2r4p11.hive.fi>
2024-11-05 17:05:55 +0200

    add new function

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ekashirs <ekashirs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 16:07:57 by ekashirs          #+#    #+#             */
-/*   Updated: 2024/11/05 15:01:16 by ekashirs         ###   ########.fr       */
+/*   Updated: 2024/11/05 17:03:31 by ekashirs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	int		start;
+	int		end;
+	char	*str;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[start]) && start <= end)
+		start++;
+	if (start > end)
+		return (ft_strdup(s1 + end + 1));
+	while (ft_strchr(set, s1[end]) && end >= 0)
+		end--;
+	str = malloc(end - start + 2);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, &s1[start], end - start + 2);
+	return (str);
 }
