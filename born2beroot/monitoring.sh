d27ff3f421dacd72cef1c57b6d2274e51a6c771a
Jane Years <janeletoxx@gmail.com>
2024-11-20 20:32:30 +0200

    add new lines in monitoring.sh

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -9,6 +9,10 @@ RAM_PERCENT=$(free --mega | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
 DISK_TOTAL=$(df -h --total | grep total | awk '{print $?}')
 DISK_USEDL=$(df -h --total | grep total | awk '{print $?}')
 DISK_PERCENT=$(df -h --total | grep total | awk '{print $?}')
+CPU_UT=$(top -bn 1 | grep 'Cpu' | awk {'printf("%.1f%%", $2 + $4)'})
+REBOOT=$(who -b | awk '{printf("%s %s"), $3, $4}')
+LVM_NUMBER=$(lsblk | grep -c 'lvm')
+LVM_USE=$(if [ $LVM_NUMBER -eq 0 ]; then echo no; else echo yes; fi)
 
 wall "
     # Architecture: $ARCHI
@@ -16,9 +20,9 @@ wall "
     # Number of virtual CPUs: $VCPU
     # Memort usage: $RAM_USED / $RAM_TOTAL MB ($RAM_PERCENT)
     # Disk usage: $DISK_USED / $DISK_TOTAL GB ($DISK_PERCENT)
-    # CPU load:
-    # Last reboot:
-    # LVM use:
+    # CPU load:$CPU_UT
+    # Last reboot: $REBOOT
+    # LVM use: $LVM_USE
     # Number of active connections:
     # Logged users:
     # Nerwork:
