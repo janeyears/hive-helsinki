52502978cdb97d4aa2723ea2f3cdadc395fa2f18
Evgeniia Kashirskaia <ekashirs@c2r4p11.hive.fi>
2024-11-21 10:50:55 +0200

    Divide projects

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -24,6 +24,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		i++;
 	}
 	if (i != n)
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		return (s1[i] - s2[i]);
 	return (0);
 }
